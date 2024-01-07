0a33bdd32b27ead9c98f3ef4f09ab0e83d7729f4
Mateus Ito <mateusito10@gmail.com>
2024-01-07 07:41:37 -0300

    chore(lint): disable no-var-requires for cjs files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,4 +28,12 @@ module.exports = {
       },
     ],
   },
+  overrides: [
+    {
+      files: ['*.cjs'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 };
