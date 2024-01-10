f884be69a75231cbb15a2d1ac439238c92286f9c
Mateus Ito <mateusito10@gmail.com>
2024-01-10 08:08:58 -0300

    chore(styles): setup unocss icons preset

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-  <NuxtWelcome />
+  <div h-screen center gap-2>
+    <h1 text-4xl>Nuxt Template</h1>
+
+    <div i-mdi:github w-12 h-12></div>
+  </div>
 </template>
--- uno.config.ts
+++ uno.config.ts
@@ -1,13 +1,20 @@
 import {
   defineConfig,
   presetAttributify,
+  presetIcons,
   presetUno,
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss';
 
 export default defineConfig({
-  presets: [presetUno(), presetAttributify()],
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons({
+      autoInstall: true,
+    }),
+  ],
   transformers: [transformerDirectives(), transformerVariantGroup()],
   shortcuts: {
     center: 'flex items-center justify-center',
