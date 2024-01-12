5d873bbfdd8d058ea14641e8f034ff30a61c3cd8
Mateus Ito <mateusito10@gmail.com>
2024-01-11 21:28:25 -0300

    chore(home): add link to github repo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,8 @@
   <div h-screen center gap-2>
     <h1 text-4xl>Nuxt Template</h1>
 
-    <div i-mdi:github w-12 h-12></div>
+    <a href="https://github.com/jos620/nuxt-template" target="_blank">
+      <div i-mdi:github w-12 h-12></div>
+    </a>
   </div>
 </template>
