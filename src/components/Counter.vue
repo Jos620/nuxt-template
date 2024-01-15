9aadc37ecbc9a240c5c3f582bed4a5819f93264c
Mateus Ito <mateusito10@gmail.com>
2024-01-14 21:15:49 -0300

    chore: add counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const store = useCountStore();
+</script>
+
+<template>
+  <button
+    bg="indigo-500 hover:indigo-600"
+    p="x-4 y-2"
+    rounded
+    cursor-pointer
+    @click="store.increment"
+  >
+    {{ store.count }}
+  </button>
+</template>
