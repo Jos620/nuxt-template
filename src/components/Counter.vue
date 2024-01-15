2263aa359ce4e145262b23908bfc976d032fec4a
Mateus Ito <mateusito10@gmail.com>
2024-01-15 10:38:49 -0300

    chore: add initialCount prop to `Counter` component

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
+interface Props {
+  initialCount?: number;
+}
+const { initialCount = 0 } = defineProps<Props>();
+
 const store = useCountStore();
+
+store.count = initialCount;
 </script>
 
 <template>
