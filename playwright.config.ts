9af29d5cf810644ec787f279ffd3790ca4771da0
Mateus Ito <mateusito10@gmail.com>
2024-01-17 15:47:11 -0300

    chore(tests): change playwright report output folders

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,11 +2,13 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/e2e',
+  outputDir: './tests/e2e/results',
+  snapshotDir: './tests/e2e/snapshots',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { outputFolder: './tests/e2e/report' }]],
   use: {
     baseURL: 'http://127.0.0.1:3000',
     trace: 'on-first-retry',
