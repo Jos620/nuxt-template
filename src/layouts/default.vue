50019ecb86269fcd2d2599134f3baabe630b9c2a
Mateus Ito <mateusito10@gmail.com>
2024-05-17 23:59:05 -0300

    refactor(layouts): move layout logic to layout component

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+const isDark = useDark();
+</script>
+
 <template>
-  <slot />
+  <div h-svh relative col>
+    <main h-full center col="<sm:~" gap-2 px-4>
+      <slot />
+    </main>
+
+    <div absolute bottom-4 right-4 center gap-4>
+      <Counter />
+
+      <ClientOnly>
+        <UiButton @click="() => (isDark = !isDark)">
+          <div v-if="isDark" i-mdi:weather-night />
+          <div v-else i-mdi:white-balance-sunny />
+        </UiButton>
+      </ClientOnly>
+    </div>
+  </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,54 +1,43 @@
 <script setup lang="ts">
 import type { TestResponse } from '@/server/api/test';
 
-const { data } = useFetch<TestResponse>('/api/test');
+const app = useNuxtApp();
 
-const isDark = useDark();
+const { data } = useFetch<TestResponse>('/api/test', {
+  getCachedData: (key) => app.payload.data[key],
+});
 
 const githubRepoLink = 'https://github.com/jos620/nuxt-template';
 const githubRepoName = githubRepoLink.split('/').slice(-2).join('/');
 </script>
 
 <template>
-  <main h-svh center col="<sm:~" gap-2 relative>
-    <h1>{{ data?.title }}</h1>
+  <h1>{{ data?.title }}</h1>
 
-    <a class="sm:hidden" :href="githubRepoLink" target="_blank">
-      <div i-mdi:github w-12 h-12></div>
-    </a>
+  <a class="sm:hidden" :href="githubRepoLink" target="_blank">
+    <div i-mdi:github w-12 h-12></div>
+  </a>
 
-    <UiHoverCard :open-delay="125">
-      <UiHoverCardTrigger as-child>
-        <a class="<sm:hidden" :href="githubRepoLink" target="_blank">
-          <div i-mdi:github w-12 h-12></div>
-        </a>
-      </UiHoverCardTrigger>
-      <UiHoverCardContent as-child>
-        <a :href="githubRepoLink" flex items-center gap-4>
-          <div center gap-4 h-9>
-            <div i-mdi:github w-8 h-8></div>
-            <UiSeparator orientation="vertical" />
-          </div>
+  <UiHoverCard :open-delay="125">
+    <UiHoverCardTrigger as-child>
+      <a class="<sm:hidden" :href="githubRepoLink" target="_blank">
+        <div i-mdi:github w-12 h-12></div>
+      </a>
+    </UiHoverCardTrigger>
+    <UiHoverCardContent as-child>
+      <a :href="githubRepoLink" flex items-center gap-4>
+        <div center gap-4 h-9>
+          <div i-mdi:github w-8 h-8></div>
+          <UiSeparator orientation="vertical" />
+        </div>
 
-          <div col justify-center gap="2px">
-            <p text-small>
-              {{ githubRepoName }}
-            </p>
-            <span text-muted>GitHub</span>
-          </div>
-        </a>
-      </UiHoverCardContent>
-    </UiHoverCard>
-
-    <div absolute bottom-4 right-4 center gap-4>
-      <Counter />
-
-      <ClientOnly>
-        <UiButton @click="() => (isDark = !isDark)">
-          <div v-if="isDark" i-mdi:weather-night />
-          <div v-else i-mdi:white-balance-sunny />
-        </UiButton>
-      </ClientOnly>
-    </div>
-  </main>
+        <div col justify-center gap="2px">
+          <p text-small>
+            {{ githubRepoName }}
+          </p>
+          <span text-muted>GitHub</span>
+        </div>
+      </a>
+    </UiHoverCardContent>
+  </UiHoverCard>
 </template>
